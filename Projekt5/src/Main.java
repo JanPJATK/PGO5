063ca12b6f4f2be8768e000348586794397c721a
Jan Przedpełski <s32026@pjwstk.edu.pl>
2024-05-19 14:34:42 +0200

    Projekt5

--- Projekt5/src/Main.java
+++ Projekt5/src/Main.java
@@ -1,17 +1,12 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+Student s=new Student("John", "Doe", "doe@wp.pl", "Warsaw,
+        Zlota 12", "333-322-222", new Date(1980, 1, 1)); " +
+        "StudyProgramme it=new StudyProgramme("IT", "AAA", 7, 5);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        s.enrollStudent(it);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-    }
-}
+s.addGrade(5, "PGO");
+s.addGrade(2, "APBD");
+
+        Students.promoteAllStudents();
+
+        Students.displayInfoAboutAllStudents()
